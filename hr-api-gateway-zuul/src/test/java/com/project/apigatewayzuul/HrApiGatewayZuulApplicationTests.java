bb287a253fd14b9d20d699eb977c2ba8052949ac
Ildson Moraes <ildsonmoraes@MacBook-Air-de-Ildson.local>
2021-01-10 16:39:42 -0300

    api gateway zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/project/apigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
