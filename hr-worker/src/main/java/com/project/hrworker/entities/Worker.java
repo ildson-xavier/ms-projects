7cc9e49ddd7621c389fd269c6baa415161a0ba09
Ildson Moraes <ildsonmoraes@MacBook-Air-de-Ildson.local>
2021-01-10 11:02:48 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/project/hrworker/entities/Worker.java
@@ -0,0 +1,52 @@
+package com.project.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable{
+
+	private static final long serialVersionUID = 6144582048652474038L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+	
+	public Worker() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+	
+	
+}
